d7090044cafef31d1e07da2a11e1d142901fc1cd
Sagar <sagarkpatel179@gmail.com>
2024-07-22 20:06:29 +0530

    Weather Dynamic

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrl: './weather.component.scss'
 })
 export class WeatherComponent {
+  apiUrl: any = 'https://cloud-api.up.railway.app/';
+  weatherData: any;
 
+  constructor(private http: HttpClient) {
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.http.get(`${this.apiUrl}weather`).subscribe(res => {
+      if (res)
+        this.weatherData = res;
+    });
+  }
 }
